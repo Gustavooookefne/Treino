2bc96ae8718491ec86f5d1be18c3e3df671761ce
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-13 18:53:00 -0300

    feat: Criando a conexao com o banco de dados

---
+++ Treino/Treino/src/main/java/com/teste/Treino/db/Conexao.java
@@ -0,0 +1,17 @@
+package com.teste.Treino.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PSSW = "";
+
+    public Connection connection()throws SQLException{
+        return DriverManager.getConnection(URL, USER, PSSW);
+    }
+
+}
--- Treino/Treino/src/main/java/com/teste/Treino/repository/UsuarioRepository.java
+++ Treino/Treino/src/main/java/com/teste/Treino/repository/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package com.teste.Treino.repository;
 
+import com.teste.Treino.model.Usuario;
+
+import java.sql.SQLException;
+
 public class UsuarioRepository {
+
+    public Usuario criarUsuario (Usuario usuario)throws SQLException{
+
+    }
 }
