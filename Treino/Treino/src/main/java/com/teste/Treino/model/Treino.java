feb4a691be10898b4c13e54eea043a2abda70729
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-13 18:44:42 -0300

    feat: Finalizando A class Treino

--- Treino/Treino/src/main/java/com/teste/Treino/model/Treino.java
+++ Treino/Treino/src/main/java/com/teste/Treino/model/Treino.java
@@ -1,4 +1,43 @@
 package com.teste.Treino.model;
 
 public class Treino {
+
+    private long id;
+    private String tipo;
+    private String descricao;
+
+    public Treino(long id, String tipo, String descricao) {
+        this.id = id;
+        this.tipo = tipo;
+        this.descricao = descricao;
+    }
+
+    public Treino(String tipo, String descricao) {
+        this.tipo = tipo;
+        this.descricao = descricao;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
