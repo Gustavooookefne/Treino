6660eda2ab5d573a9d88ccc43fb75c5745068043
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-13 18:45:33 -0300

    feat: Finalizando A class Dieta

--- Treino/Treino/src/main/java/com/teste/Treino/model/Dieta.java
+++ Treino/Treino/src/main/java/com/teste/Treino/model/Dieta.java
@@ -1,4 +1,43 @@
 package com.teste.Treino.model;
 
 public class Dieta {
+
+    private long id;
+    private String nomeDieta;
+    private String detalhesDieta;
+
+    public Dieta(long id, String nomeDieta, String detalhesDieta) {
+        this.id = id;
+        this.nomeDieta = nomeDieta;
+        this.detalhesDieta = detalhesDieta;
+    }
+
+    public Dieta(String nomeDieta, String detalhesDieta) {
+        this.nomeDieta = nomeDieta;
+        this.detalhesDieta = detalhesDieta;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNomeDieta() {
+        return nomeDieta;
+    }
+
+    public void setNomeDieta(String nomeDieta) {
+        this.nomeDieta = nomeDieta;
+    }
+
+    public String getDetalhesDieta() {
+        return detalhesDieta;
+    }
+
+    public void setDetalhesDieta(String detalhesDieta) {
+        this.detalhesDieta = detalhesDieta;
+    }
 }
