8995e17fcc7cff1c18f0e3f2df06fd38a1f47381
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-16 16:55:54 -0300

    feat: Finalizando o DietaService

--- Treino/Treino/src/main/java/com/teste/Treino/service/DietaService.java
+++ Treino/Treino/src/main/java/com/teste/Treino/service/DietaService.java
@@ -1,4 +1,38 @@
 package com.teste.Treino.service;
 
+import com.teste.Treino.Dto.DietaDtos.DietaRequestDto;
+import com.teste.Treino.model.Dieta;
+import com.teste.Treino.repository.DietaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class DietaService {
+
+    private final DietaRepository repository;
+
+    public Dieta salvar (DietaRequestDto dto) {
+        Dieta dieta = new Dieta();
+
+        dieta.setDetalhesDieta(dto.detalhesDieta());
+        dieta.setNomeDieta(dto.nomeDieta());
+
+        return repository.save(dieta);
+    }
+
+    public List<Dieta> listarTodos () {
+        return repository.findAll();
+    }
+
+    public Dieta listarPorId (long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Lista não encontrada"));
+    }
+
+    public void deletar (long id){
+        repository.deleteById(id);
+    }
 }
