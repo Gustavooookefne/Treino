c968419c34baaac693c79e266d6d0adeb9fe5ec2
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-16 16:48:47 -0300

    feat: Finalizando o TreinoService

--- Treino/Treino/src/main/java/com/teste/Treino/service/TreinoService.java
+++ Treino/Treino/src/main/java/com/teste/Treino/service/TreinoService.java
@@ -1,4 +1,39 @@
 package com.teste.Treino.service;
 
+import com.teste.Treino.Dto.TreinoDtos.TreinoRequestDto;
+import com.teste.Treino.model.Treino;
+import com.teste.Treino.repository.TreinoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class TreinoService {
+
+    private final TreinoRepository repository;
+
+    public Treino salvar(TreinoRequestDto dto){
+        Treino treino = new Treino();
+
+        treino.setDescricao(dto.descricao());
+        treino.setTipo(dto.tipo());
+
+        return repository.save(treino);
+    }
+
+    public List<Treino> listarTreinos(){
+        return repository.findAll();
+    }
+
+    public Treino listarPorId(long id){
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Lista não encontrada" + id));
+    }
+
+    public void deletarTreino (long id){
+        repository.deleteById(id);
+    }
+
 }
