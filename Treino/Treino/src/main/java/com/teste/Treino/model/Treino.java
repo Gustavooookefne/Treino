436d32498f36b0139d23bbc3f9d2404d81488af1
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-16 15:54:58 -0300

    feat:finalizado o TreinoModel

--- Treino/Treino/src/main/java/com/teste/Treino/model/Treino.java
+++ Treino/Treino/src/main/java/com/teste/Treino/model/Treino.java
@@ -1,43 +1,23 @@
 package com.teste.Treino.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Treino {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String tipo;
     private String descricao;
 
-    public Treino(long id, String tipo, String descricao) {
-        this.id = id;
-        this.tipo = tipo;
-        this.descricao = descricao;
-    }
-
-    public Treino(String tipo, String descricao) {
-        this.tipo = tipo;
-        this.descricao = descricao;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 }
