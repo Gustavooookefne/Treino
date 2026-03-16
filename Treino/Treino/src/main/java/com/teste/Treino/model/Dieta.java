06da8023862bd973038b46631f452a1c9bd5915e
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-16 15:55:49 -0300

    feat:finalizado o DietaModel

--- Treino/Treino/src/main/java/com/teste/Treino/model/Dieta.java
+++ Treino/Treino/src/main/java/com/teste/Treino/model/Dieta.java
@@ -1,43 +1,24 @@
 package com.teste.Treino.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Dieta {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nomeDieta;
     private String detalhesDieta;
 
-    public Dieta(long id, String nomeDieta, String detalhesDieta) {
-        this.id = id;
-        this.nomeDieta = nomeDieta;
-        this.detalhesDieta = detalhesDieta;
-    }
 
-    public Dieta(String nomeDieta, String detalhesDieta) {
-        this.nomeDieta = nomeDieta;
-        this.detalhesDieta = detalhesDieta;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNomeDieta() {
-        return nomeDieta;
-    }
-
-    public void setNomeDieta(String nomeDieta) {
-        this.nomeDieta = nomeDieta;
-    }
-
-    public String getDetalhesDieta() {
-        return detalhesDieta;
-    }
-
-    public void setDetalhesDieta(String detalhesDieta) {
-        this.detalhesDieta = detalhesDieta;
-    }
 }
