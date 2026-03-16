b403d81e28d0c2b4bc366e6c71ea2e6238c8b7c7
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-16 15:53:18 -0300

    feat: Adicionando JPA no meu projeto finalizado o UsuarioModel

--- Treino/Treino/src/main/java/com/teste/Treino/model/Usuario.java
+++ Treino/Treino/src/main/java/com/teste/Treino/model/Usuario.java
@@ -1,81 +1,28 @@
 package com.teste.Treino.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
 
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
     private String nome;
     private String email;
     private int idade;
     private double peso;
     private double altura;
 
-    public Usuario(long id, String nome, String email, int idade, double peso, double altura) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-        this.peso = peso;
-        this.altura = altura;
-    }
-
-    public Usuario(String nome, String email, int idade, double peso, double altura) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-        this.peso = peso;
-        this.altura = altura;
-    }
-
-    public Usuario() {
-
-    }
-
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public double getPeso() {
-        return peso;
-    }
-
-    public void setPeso(double peso) {
-        this.peso = peso;
-    }
-
-    public double getAltura() {
-        return altura;
-    }
-
-    public void setAltura(double altura) {
-        this.altura = altura;
-    }
 }
