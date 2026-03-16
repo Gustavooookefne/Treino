a377cede663683966aaebb4a5a560735778a6819
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-16 16:39:50 -0300

    feat: Finalizado o UsurioService

--- Treino/Treino/src/main/java/com/teste/Treino/service/UsuarioService.java
+++ Treino/Treino/src/main/java/com/teste/Treino/service/UsuarioService.java
@@ -1,5 +1,45 @@
 package com.teste.Treino.service;
 
+import com.teste.Treino.Dto.UsuarioDtos.UsuarioRequestDto;
+import com.teste.Treino.model.Usuario;
+import com.teste.Treino.repository.UsuarioRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
 public class UsuarioService {
+
+    private final UsuarioRepository repository;
+
+    public Usuario salvar (UsuarioRequestDto dto){
+        Usuario usuario = new Usuario();
+
+        usuario.setNome(dto.nome());
+        usuario.setEmail(dto.email());
+        usuario.setIdade(dto.idade());
+        usuario.setPeso(dto.peso());
+        usuario.setAltura(dto.altura());
+
+        return repository.save(usuario);
+
+    }
+
+
+    public List<Usuario> listarTodos(){
+        return repository.findAll();
+    }
+
+    public Usuario listarPorId(UUID id){
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuario não encontrado" + id));
+    }
+
+    public void deletar (UUID id){
+        repository.deleteById(id);
+    }
 
 }
