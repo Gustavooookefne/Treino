edeab40078b36ab628da9dcc4f085f48fc27ff90
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-17 16:45:02 -0300

    feat: Finalizado o TreinoController

--- Treino/Treino/src/main/java/com/teste/Treino/controller/TreinoController.java
+++ Treino/Treino/src/main/java/com/teste/Treino/controller/TreinoController.java
@@ -1,4 +1,44 @@
 package com.teste.Treino.controller;
 
+import com.teste.Treino.dto.TreinoDtos.TreinoRequestDto;
+import com.teste.Treino.model.Treino;
+import com.teste.Treino.service.TreinoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/treino")
 public class TreinoController {
+
+    private final TreinoService service;
+
+    public TreinoController(TreinoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Treino salvar (@RequestBody TreinoRequestDto treinoRequestDto){
+        return service.salvar(treinoRequestDto);
+    }
+
+    @GetMapping
+    public List<Treino> listarTodos(){
+        return service.listarTreinos();
+    }
+
+    @GetMapping("/{id}")
+    public Treino listarPorId (@PathVariable long id) {
+        return service.listarPorId(id);
+    }
+
+    @PutMapping("/{id}")
+    public Treino atualizar (@RequestBody TreinoRequestDto treinoRequestDto , @PathVariable long id){
+        return service.atualizar(id, treinoRequestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar (@PathVariable long id){
+        service.deletarTreino(id);
+    }
 }
